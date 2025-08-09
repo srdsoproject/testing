6b9a1b761ec5cf9d1dd5613145806f12f4c4220a
srdsoproject <srdsosurproject@gmail.com>
2025-08-09 21:48:19 +0530

    Update main.py

--- main.py
+++ main.py
@@ -620,13 +620,7 @@ with tabs[0]:
 def get_status(feedback, remark):
     status = classify_feedback(feedback, remark)  # tumhara existing function
     return status
-def color_status(val):
-    if val == "Pending":
-        return 'color: red; font-weight: bold'
-    elif val == "Resolved":
-        return 'color: green; font-weight: bold'
-    else:
-        return ''
+
 
 styled_df = editable_df.style.applymap(color_status, subset=['Status'])
 st.markdown("### ✍️ Edit User Feedback/Remarks in Table")
@@ -634,6 +628,13 @@ st.markdown("### ✍️ Edit User Feedback/Remarks in Table")
 editable_filtered = filtered.copy()
 
 if not editable_filtered.empty:
+    def color_status(val):
+    if val == "Pending":
+        return 'color: red; font-weight: bold'
+    elif val == "Resolved":
+        return 'color: green; font-weight: bold'
+    else:
+        return ''
     if "_sheet_row" not in editable_filtered.columns:
         editable_filtered["_sheet_row"] = editable_filtered.index + 2  
 
@@ -733,6 +734,7 @@ if not editable_filtered.empty:
                         st.info("ℹ️ No changes detected to save.")
                 else:
                     st.warning("⚠️ No rows matched for update.")
+
 
 
 
